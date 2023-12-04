145074a8211020743c65647ccabb0dc22a54bcb4
Jas667 <100931408+Jas667@users.noreply.github.com>
2023-12-04 13:56:59 +0000

    alert added

--- App.js
+++ App.js
@@ -6,15 +6,21 @@ import {
   Button,
   Pressable,
   Modal,
+  StatusBar,
+  ActivityIndicator,
+  Alert,
 } from "react-native";
 import { useState } from "react";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isStatusHidden, setIsStatusHidden] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <StatusBar style="auto" hidden={isStatusHidden} />
       {/* <Text>
         <Text style={{ color: "white" }}>Hello </Text>
         World!
@@ -24,9 +30,16 @@ export default function App() {
         onPress={() => setIsModalVisible(true)}
         color="midnightblue"
       />
-      <Pressable onPress={() => setIsModalVisible(true)}>
+      <ActivityIndicator size="large" color="black" animating={isLoading} />
+      <Pressable onPress={() => setIsStatusHidden(!isStatusHidden)}>
         <Image source={logoImg} style={{ width: 300, height: 300 }} />
       </Pressable>
+      <Button title="Loading" color="midnightblue" onPress={() => setIsLoading(!isLoading)} />
+      <Button title="Alert" color="midnightblue" onPress={() => Alert.alert("Invalid data!", "DOB Incorrect", [
+        { text: "Cancel", onPress: () => console.log("Cancel Pressed"), style: "cancel" },
+        { text: "OK", onPress: () => console.log("OK Pressed"), style: "cancel" },
+
+      ])} />
       {/* <Image source={{uri: "https://picsum.photos/300"}} style={{ width: 300, height: 300 }} /> */}
       {/* <ImageBackground source={logoImg} style={{ flex: 1}}>
         <Text>IMAGE TEXT</Text>
@@ -35,6 +48,7 @@ export default function App() {
         <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
           <Text>MODAL TEXT</Text>
           <Button title="Close" color="midnightblue" onPress={() => setIsModalVisible(false)} />
+
         </View>
       </Modal>
     </View>
