696a9ed7ed0d0159c7bbd7731f2e4ea395928f00
Jas667 <100931408+Jas667@users.noreply.github.com>
2023-12-05 08:33:52 +0000

    styling practice

--- App.js
+++ App.js
@@ -1,56 +1,24 @@
-import {
-  View,
-  Text,
-  Image,
-  ImageBackground,
-  Button,
-  Pressable,
-  Modal,
-  StatusBar,
-  ActivityIndicator,
-  Alert,
-} from "react-native";
-import { useState } from "react";
-const logoImg = require("./assets/adaptive-icon.png");
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [isStatusHidden, setIsStatusHidden] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-
   return (
-    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <StatusBar style="auto" hidden={isStatusHidden} />
-      {/* <Text>
-        <Text style={{ color: "white" }}>Hello </Text>
-        World!
-      </Text> */}
-      <Button
-        title="Press"
-        onPress={() => setIsModalVisible(true)}
-        color="midnightblue"
-      />
-      <ActivityIndicator size="large" color="black" animating={isLoading} />
-      <Pressable onPress={() => setIsStatusHidden(!isStatusHidden)}>
-        <Image source={logoImg} style={{ width: 300, height: 300 }} />
-      </Pressable>
-      <Button title="Loading" color="midnightblue" onPress={() => setIsLoading(!isLoading)} />
-      <Button title="Alert" color="midnightblue" onPress={() => Alert.alert("Invalid data!", "DOB Incorrect", [
-        { text: "Cancel", onPress: () => console.log("Cancel Pressed"), style: "cancel" },
-        { text: "OK", onPress: () => console.log("OK Pressed"), style: "cancel" },
-
-      ])} />
-      {/* <Image source={{uri: "https://picsum.photos/300"}} style={{ width: 300, height: 300 }} /> */}
-      {/* <ImageBackground source={logoImg} style={{ flex: 1}}>
-        <Text>IMAGE TEXT</Text>
-      </ImageBackground> */}
-      <Modal visible={isModalVisible} animationType="slide" onRequestClose={() => setIsModalVisible(false)} >
-        <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
-          <Text>MODAL TEXT</Text>
-          <Button title="Close" color="midnightblue" onPress={() => setIsModalVisible(false)} />
-
-        </View>
-      </Modal>
+    <View style={styles.container}>
+      <Text style={styles.title}>StyleSheet API</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'plum',
+    padding: 60,
+  },
+  title: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 50,
+    fontWeight: 'bold',
+  },
+});
