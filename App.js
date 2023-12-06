bfe1e5a88de36c100bf261bd2e95816e4e27aa44
Jas667 <100931408+Jas667@users.noreply.github.com>
2023-12-06 12:01:23 +0000

    dynamic styling tutorial

--- App.js
+++ App.js
@@ -1,76 +1,33 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, Text, Dimensions } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.darkMode}>
-        <Text style={styles.darkModeText}>Style Inheritance</Text>
-      </View>
-      <View>
-        <Text
-          style={[
-            styles.lightBlueBg,
-            styles.box,
-            styles.boxShadow,
-            styles.shadowColor,
-          ]}
-        >
-          Light blue box
-        </Text>
-      </View>
-      <View>
-        <Text
-          style={[
-            styles.lightGreenBg,
-            styles.box,
-            styles.androidBoxShadow,
-            styles.shadowColor,
-          ]}
-        >
-          Light green box
-        </Text>
+      <View style={styles.box}>
+        <Text style={styles.text}>Welcome</Text>
       </View>
     </View>
   );
 }
 
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "plum",
-    padding: 60,
-  },
-  darkMode: {
-    backgroundColor: "black",
-  },
-  darkModeText: {
-    color: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
   box: {
-    padding: 10,
-    width: 250,
-    height: 250,
-    marginVertical: 20,
-    borderColor: "black",
-    borderWidth: 1,
-    borderRadius: 10,
-  },
-  lightBlueBg: {
+    width: "70%",
+    height: "40%",
     backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  lightGreenBg: {
-    backgroundColor: "lightgreen",
-  },
-  boxShadow: {
-    shadowOffset: { width: 6, height: 6 },
-    shadowOpacity: 0.6,
-    shadowRadius: 4,
-  },
-  androidBoxShadow: {
-    elevation: 10,
-  },
-  shadowColor: {
-    shadowColor: "blue",
+  text: {
+    fontSize: 24,
   },
 });
