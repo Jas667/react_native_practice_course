486d0a0a04e8a99c046d5334a8559b5da67ff109
Jas667 <100931408+Jas667@users.noreply.github.com>
2023-12-13 09:59:57 +0000

    pokemon card part 1

--- App.js
+++ App.js
@@ -1,46 +1,18 @@
-import { StyleSheet, View, Text, SafeAreaView, Platform } from "react-native";
+import { StyleSheet, SafeAreaView, Platform } from "react-native";
+import PokemonCard from "./components/PokemonCard";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.safeContainer}>
-      <View style={styles.container}>
-        <View style={styles.box}>
-          <Text style={styles.text}>Welcome!</Text>
-        </View>
-      </View>
+    <SafeAreaView style={styles.container}>
+      <PokemonCard />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  safeContainer: {
-    flex: 1,
-    backgroundColor: "plum",
-  },
   container: {
     flex: 1,
-    backgroundColor: "plum",
-    paddingTop: Platform.OS === "android" ? 25 : 0,
-  },
-  box: {
-    padding: 20,
-  },
-  text: {
-    ...Platform.select({
-      ios: {
-        color: "red",
-        fontSize: 20,
-      },
-      android: {
-        color: "blue",
-        fontSize: 30,
-      },
-      default: {
-        color: "green",
-        fontSize: 20,
-      },
-    }),
-    fontWeight: "bold",
-    textAlign: "center",
+    backgroundColor: "#f5f5f5",
+    paddingTop: Platform.OS === "android" ? 35 : 0,
   },
 });
