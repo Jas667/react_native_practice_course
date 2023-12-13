5330c7b67489610dd441f0764786a2c9cb940397
Jas667 <100931408+Jas667@users.noreply.github.com>
2023-12-13 10:24:03 +0000

    create card

--- App.js
+++ App.js
@@ -2,9 +2,18 @@ import { StyleSheet, SafeAreaView, Platform } from "react-native";
 import PokemonCard from "./components/PokemonCard";
 
 export default function App() {
+  const squData = {
+    name: "Squirtle",
+    image: require("./assets/Image1.png"),
+    type: "Air",
+    hp: 70,
+    moves: ["Tackle", "Tail Whip", "Water Gun", "Bubble"],
+    weaknesses: ["Electric", "Grass"],
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <PokemonCard />
+      <PokemonCard {...squData} />
     </SafeAreaView>
   );
 }
--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -1,9 +1,54 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Platform, Image } from "react-native";
 
-export default function PokemonCard() { 
-      return (
+export default function PokemonCard({
+  name,
+  image,
+  type,
+  hp,
+  moves,
+  weaknesses,
+}) {
+  return (
+    <View style={styles.card}>
       <View>
-            <Text>Pokemon Card</Text>
+        <Text>{name}</Text>
+        <Text>{hp}</Text>
       </View>
-      )
+      <Image source={image} accessibilityLabel={`${name} pokemon`} />
+      <View>
+        <Text>{type}</Text>
+      </View>
+
+      <View>
+        <Text>Moves: {moves.join(", ")}</Text>
+      </View>
+      <View>
+        <Text>Weaknesses: {weaknesses.join(", ")}</Text>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+    borderRadius: 16,
+    borderWidth: 2,
+    padding: 16,
+    margin: 16,
+    ...Platform.select({
+      ios: {
+        shadowColor: "#333",
+        shadowOffset: {
+          width: 2,
+          height: 2,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+});
